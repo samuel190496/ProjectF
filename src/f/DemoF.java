5e1477e0329ff18968a5e67ca6eb17ceec663b7f
Samuel <Samuel@192.168.0.21>
2024-01-05 15:25:22 -0800

    initial commit

---
+++ src/f/DemoF.java
@@ -0,0 +1,12 @@
+package f;
+
+public class DemoF {
+	public static void main(String[] args) {
+		System.out.println("edris wedi haji");
+		
+		System.out.println("the last dance of java");
+		System.out.println("happy new year ");
+	}
+
+}
+
